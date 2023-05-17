9143654d3a5515e515d606ab0575443239a56793
Vishal-Merugu <buntymerugu0@gmail.com>
2023-05-17 13:01:09 +0530

    created user model save and findById methods

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require("./util/database").mongoConnect
+const mongoConnect = require("./util/database").mongoConnect;
+
+const User = require('./models/user')
 
 const app = express();
 
@@ -17,14 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById(1)
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("6464809fc4d4785c44fbe8c6")
+    .then(user => {
+      req.user = user;
+      // console.log(user);
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
