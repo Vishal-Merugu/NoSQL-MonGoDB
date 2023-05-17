7f5c0bae5bfc8263e47d0a29975b66ff9a711f1a
Vishal-Merugu <buntymerugu0@gmail.com>
2023-05-17 21:17:49 +0530

    get cart with product and quantity objs of a arry

--- models/user.js
+++ models/user.js
@@ -56,8 +56,28 @@ class User{
     .catch(err => {
       console.log(err);
     })
-
   }  
+
+  getCart(){
+    const db = getDb();
+    const productIds = this.cart.items.map(product => {
+      return product.productId 
+    })
+    return db.collection('products').find({ _id : { $in :productIds }})
+    .toArray()
+    .then(products => {
+      return products.map(p => {
+        return {
+          ...p,
+          quantity : this.cart.items.find(i => {
+            return i.productId.toString() === p._id.toString()
+        })}.quantity
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 
 }
 
